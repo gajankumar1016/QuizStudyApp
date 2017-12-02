d9b1d33413020e2134b6e3b45d12c8fef73fce52
Gajan Kumar <gajankumar1016@gmail.com>
2017-12-02 15:32:04 -0600

    Created a ViewCoursesActivity in which users can view their courses.

--- app/src/main/java/edu/illinois/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/finalproject/MainActivity.java
@@ -1,13 +1,30 @@
 package edu.illinois.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goToCoursesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToCoursesButton = (Button) findViewById(R.id.goToCoursesButton);
+        goToCoursesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent viewCoursesIntent = new Intent(context, ViewCoursesActivity.class);
+                startActivity(viewCoursesIntent);
+            }
+        });
+
     }
 }
